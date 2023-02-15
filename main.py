ed567b3e4f443efead3dcac5bf8ae0bf394400e1
lugboard <denebola237@gmail.com>
2023-02-15 23:03:20 +0900

    a

---
+++ main.py
@@ -0,0 +1,12 @@
+import serial
+import time
+import ambient
+
+co2 = serial.Serial('dev/ttyACM0', 115200)
+
+co2.write('STA')
+while True:
+    ans = co2.read()
+    print(ans)
+co2.write('STP')
+co2.close()
