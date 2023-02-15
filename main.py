95a6b52b7892caeb9eb0daf0bc9108a091734516
lugboard <denebola237@gmail.com>
2023-02-15 23:51:42 +0900

    aa

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import serial
 import time
 #import ambient
 
-co2 = serial.Serial('dev/ttyACM0', 115200)
+co2 = serial.Serial('/dev/ttyACM0', 115200)
 
-co2.write('STA')
-while True:
-    ans = co2.read()
-    print(ans)
-co2.write('STP')
+co2.write(b'STA')
+print('send STA')
+out = co2.readline() #prevent error
+out = co2.readline()
+#print(ans.decode())
+co2.write(b'STP')
+room_env = out.decode().strip().split(',')
+print(room_env)
+print('send STP')
 co2.close()
